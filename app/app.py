c1ed2211eb42183df64e8efc65cb28306c57ec4e
LaPantherine <237436235+LaPantherine@users.noreply.github.com>
2026-03-22 17:04:22 +0300

    Fix Prometheus metrics content type

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ def home():
 
 @app.route('/metrics')
 def metrics():
-    return generate_latest()
+    return generate_latest(), 200, {'Content-Type': 'text/plain'}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
